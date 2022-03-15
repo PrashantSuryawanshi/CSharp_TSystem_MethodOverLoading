8742a5fc384b90876cd790a846955c63b12394ba
Kanha Computers <Kanha Computers@DESKTOP-N4320G0>
2022-03-15 11:43:55 +0530

    Add project files.

---
+++ BasicDemo_Ex1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+
+namespace BasicDemo_Ex1
+{
+    internal class Program
+    {
+        private int id;
+        private string name, city,location;
+
+        public Program()//constructer
+        {
+            id = 11;
+            name = "IT";
+            city = "Location";
+            Console.WriteLine("u are in constructer.");
+        }
+        public Program(int did,string dname,string dcity)
+        {
+            id = did;
+            name = dname;
+            city = dcity;
+            Console.WriteLine("you are in Parameterise constructer.");
+          
+        }
+        public void AssignDate()//method
+        {
+            id = 1;
+            name = "IT";
+            location = "Pune";
+            Console.WriteLine("your in Method.");
+        }
+        public string GetDate()
+        {
+            return " Dept id :: " + id + "\nName :: " + name + "\nlocation :: " + location;
+        }
+        public string GetDate1()
+        {
+            return "Dept id :: " + id + "\nName :: " + name + "\nCity :: " + city;
+        }
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            Console.WriteLine(obj.GetDate1());
+            Console.WriteLine("=========================================");
+            obj.AssignDate();
+           // string str = obj.GeetData2();
+            Console.WriteLine(obj.GetDate1());
+            Console.WriteLine("===========================================");
+            Program obj1 = new Program(122,"Civil","dhule");
+            Console.WriteLine(obj1.GetDate1());
+            
+            Console.WriteLine("================================");
+            Students obj2 = new Students();                
+            Console.WriteLine(obj2.DisplayDetials());
+
+            Console.WriteLine("================================");
+            Students obj3 = new Students(102,"Prashant","dhule",67.43f);
+            Console.WriteLine(obj3.DisplayDetials());
+            
+            obj3.Dept(222,"Mechanical");
+            Console.WriteLine(obj3.MethodDisplay()); ;
+            Console.WriteLine("================================");
+        }
+    }
+}
---
+++ BasicDemo_Ex1/Students.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+
+namespace BasicDemo_Ex1
+{
+    internal class Students
+    {
+       private int sid,did;
+       private string sName,sadd,dname;
+       private float percent;
+
+        public Students()
+        {
+            sid = 121;
+            sName = "Ram";
+            sadd = "Pune";
+            percent = 88.23f;
+
+        }
+        public Students(int Id,string Name,string add,float mark)
+        {
+            sid = Id;
+            sName = Name;
+            sadd = add;
+            percent = mark;
+        }
+        public void Dept(int dId,string dName)
+        {
+            did = dId;
+            dname = dName;
+        }
+        public string DisplayDetials()
+        {
+            return "Students Detials ::" + "\n    Name ::" + sName + "\n    ID ::" + sid + "\n    Address ::" + sadd + "\n    Marks ::" + percent + " %";
+                 
+        }
+        public string MethodDisplay()
+        {
+            return "\nDepartment Detials ::" + "\n    DeptId ::" + did + "\n    DeptName ::" + dname;
+        }
+
+    }
+}
